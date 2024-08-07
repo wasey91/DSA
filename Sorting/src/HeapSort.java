cd8ce1ea783c8e8901ce1e9173b601898528d686
Abdul Wasey <waseya809@gmail.com>
2024-08-07 13:30:30 +0530

    alg

--- Sorting/src/HeapSort.java
+++ Sorting/src/HeapSort.java
@@ -1,18 +1,17 @@
 public class HeapSort {
     static void heapify(int a[], int n, int i)
     {
-        int largest = i; // Initialize largest as root
-        int left = 2 * i + 1; // left child
-        int right = 2 * i + 2; // right child
-        // If left child is larger than root
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
         if (left < n && a[left] > a[largest])
             largest = left;
-        // If right child is larger than root
+
         if (right < n && a[right] > a[largest])
             largest = right;
-        // If root is not largest
-        if (largest != i) {
-            // swap a[i] with a[largest]
+               if (largest != i) {
+
             int temp = a[i];
             a[i] = a[largest];
             a[largest] = temp;
@@ -20,16 +19,15 @@ public class HeapSort {
             heapify(a, n, largest);
         }
     }
-    /*Function to implement the heap sort*/
+
     static void heapSort(int a[], int n)
     {
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(a, n, i);
 
-        // One by one extract an element from heap
+
         for (int i = n - 1; i >= 0; i--) {
-            /* Move current root element to end*/
-            // swap a[0] with a[i]
+
             int temp = a[0];
             a[0] = a[i];
             a[i] = temp;
@@ -37,7 +35,7 @@ public class HeapSort {
             heapify(a, i, 0);
         }
     }
-    /* function to print the array elements */
+
     static void printArr(int a[], int n)
     {
         for (int i = 0; i < n; ++i)
